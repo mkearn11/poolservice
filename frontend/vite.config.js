cecaa2ae0b7fb46b066774ea1f5e43c29449fc5d
mkearn11 <105509418+mkearn11@users.noreply.github.com>
2025-04-15 21:49:56 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// frontend/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
-})
+});
